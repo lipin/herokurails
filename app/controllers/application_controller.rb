30bc504742c5c5581f25d29de59b8bbcbc8037e1
lipin <ping.li1688@gmail.com>
2014-08-05 17:22:30 +0800

    FIX

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,10 @@ class ApplicationController < ActionController::Base
   	ObjectSpace.each_object(klass).to_a.select {|obj| obj::__id__ == id.to_i}.first || klassy = klass.new
 	end
 	helper_method :load_game
+	# And then make sure it's loaded from the lib folder.
+	# In my case though I felt it was more at home being loaded within the controller.
+	# So in my application controller I added one line:
+
+	require Rails.root.join('lib','holdem.rb')
 
 end
